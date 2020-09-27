e6506cc4128277be2f373fb08ef6247f4a37a570
Renato <renato.szk@hotmail.com>
2020-09-27 22:12:59 +0300

    Add files via upload
    galeria de imagens com uso de drag

---
+++ gallery.js
@@ -0,0 +1,10 @@
+new Swiper(".swiper-container",
+{    
+    speed: 400,
+    spaceBetween: 100,
+    navigation: {
+        nextE1: ".swiper-button-next",
+        prevE1: ".swiper-button-prev"
+    }
+    
+});
