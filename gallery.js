fbe8e023e948a0a286f171843e95a16e34956600
Renato <renato.szk@hotmail.com>
2020-09-28 14:40:22 +0300

    final commit
    galeria finalizada com swiper e popmotion

--- gallery.js
+++ gallery.js
@@ -2,9 +2,33 @@ new Swiper(".swiper-container",
 {    
     speed: 400,
     spaceBetween: 100,
+    effect: "slide",
+    slidesPerView: 3,
+    loop: true,
     navigation: {
         nextE1: ".swiper-button-next",
         prevE1: ".swiper-button-prev"
     }
     
+});
+
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector(".brand");
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, "mousedown touchstart").start(e => {
+  e.preventDefault();
+  pointer(ballXY.get()).start(ballXY);});
+
+listen(document, "mouseup touchend").start(() => {
+  spring({
+    from: ballXY.get(),
+    velocity: ballXY.getVelocity(),
+    to: { x: 0, y: 0 },
+    stiffness: 200,
+     mass: 1,
+    damping: 10,
+  }).start(ballXY);
 });
